15fc6ed93d73411803c54aaad2d803c691a7f392
Carlos Alberto <>
2022-07-06 14:18:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+
+function calcularMaior() {
+  let numero1 = document.getElementById('numero1').value;
+  let numero2 = document.getElementById('numero2').value;
+  let p1 = document.getElementById ('p1');
+  let p2 = document.getElementById ('p2');
+  
+
+  if (numero1 > numero2) {
+    p1.classList.remove("d-none");
+  } else {
+     p2.classList.remove("d-none");
+  }
+}
